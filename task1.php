3fac3730e1b6c4a96fc43aa6b42a2d9f6676e316
Sarka <saraszewczykk@wp.pl>
2023-05-11 09:46:43 +0200

    Task 1 resolved

---
+++ task1.php
@@ -0,0 +1,22 @@
+<?php
+
+class Pipeline {
+    public static function make(...$functions) {
+        return function ($arg) use ($functions) {
+            foreach ($functions as $function) {
+            $arg = $function($arg);
+            }
+        return $arg;
+        };
+    }
+}
+
+$example = Pipeline::make(
+    function($var) { return $var * 3; },
+    function($var) { return $var + 1; },
+    function($var) { return $var / 2; }
+);
+
+echo $example(3);
+
+?>
